d986c073f58be3f31fcdd12c66604cb6f44a37bd
Chen min <34125435+Crazy-chen@users.noreply.github.com>
2018-01-13 18:26:57 +0800

    解析数据

---
+++ HTTP/http-form/qs-example.js
@@ -0,0 +1,2 @@
+console.log(require('querystring').parse('name=Chenmin'));
+console.log(require('querystring').parse('q=chenmin+rauch'));
--- HTTP/http-form/server.js
+++ HTTP/http-form/server.js
@@ -1,16 +1,26 @@
+var qs = require('querystring');
 require('http').createServer(function (req, res) {
   if ('/' == req.url) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(['<form method="POST" action="/url" >',
+    res.end([
+      '<form method="POST" action="/url" >',
       '<h1>My Form</h1>',
       '<fieldset>',
       '<label>Personal Information</label>',
       '<p>What is your name?</p>',
       '<input type="text" name="name">',
       '<p><button>Submit</button></p>',
-      '</form>'].join(''));
-  } else if ('/url' == req.url) {
-    res.writeHead(200, { 'Content-Type' : 'text/html' });
-    res.end('You send a <em>' + req.method + '</em> request');
+      '</form>'
+    ].join(''));
+  } else if ('/url' == req.url && 'POST' == req.method) {
+    var body = '';
+    req.on('data', function (chunk) {
+      body += chunk;
+    });
+    req.on('end', function () {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end('<p>Your name is <b>' + qs.parse(body).name + '</b></p>');
+    });
+
   }
 }).listen(3000);
